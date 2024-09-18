1247faa21955ed7eb85bc992cf85c51dcc1735da
Bavithran <sivabavithran16@gmail.com>
2024-09-18 22:03:18 +0530

    signup commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from "express"
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authroutes from "./routes/routes.js";
+
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hello");
-});
+const PORT = process.env.PORT || 5000;
 
-app.listen(3000, ()=>{
+app.use(express.json());
+
+app.use("/api/auth", authroutes)
+
+app.listen(PORT, ()=>{
+    connectDB();
     console.log("Server is running on port 3000")
 })
 
