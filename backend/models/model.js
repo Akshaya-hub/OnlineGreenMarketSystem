948982e2ca36d899f6fbb0268eb839d7eeebdd93
Bavithran <sivabavithran16@gmail.com>
2024-09-19 11:45:58 +0530

    new commit

---
+++ backend/models/model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    contactNo: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    otp: String, // Store OTP
+    otpExpiresAt: Date // Store OTP expiration time
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
