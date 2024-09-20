6262f58a31d38308a3b4d5f8ef768a6a2569e959
danishiya <danishiya26@gmail.com>
2024-09-21 00:41:44 +0530

    start with basic template of order page

---
+++ checkout/src/features/slice/cardSlice.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const cardSlice = () => {
+  return (
+    <div>cardSlice</div>
+  )
+}
---
+++ checkout/src/features/slice/productsApi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const productsApi = () => {
+  return (
+    <div>productsApi</div>
+  )
+}
